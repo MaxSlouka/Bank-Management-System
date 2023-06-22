be0ffda38fa2a41cbcecbc49bd592d303dfb47b1
MaxSlouka <jmslouka@gmail.com>
2023-06-22 11:31:17 +0200

    add basic sctructure for form

--- bankingsimulator/src/SignupOne.java
+++ bankingsimulator/src/SignupOne.java
@@ -29,6 +29,60 @@ public class SignupOne extends JFrame {
         setLayout(null);
         add(name);
 
+        JLabel fname = new JLabel("Father´s Name:");
+        fname.setFont(new Font("Raleway", Font.BOLD, 20));
+        fname.setBounds(100, 190, 200, 30);
+        setLayout(null);
+        add(fname);
+
+        JLabel dob = new JLabel("Date of Birth:");
+        dob.setFont(new Font("Raleway", Font.BOLD, 20));
+        dob.setBounds(100, 240, 200, 30);
+        setLayout(null);
+        add(dob);
+
+        JLabel gender = new JLabel("Gender:");
+        gender.setFont(new Font("Raleway", Font.BOLD, 20));
+        gender.setBounds(100, 290, 200, 30);
+        setLayout(null);
+        add(gender);
+
+        JLabel email = new JLabel("Email Address:");
+        email.setFont(new Font("Raleway", Font.BOLD, 20));
+        email.setBounds(100, 340, 200, 30);
+        setLayout(null);
+        add(email);
+
+        JLabel marital = new JLabel("Marital Status:");
+        marital.setFont(new Font("Raleway", Font.BOLD, 20));
+        marital.setBounds(100, 390, 200, 30);
+        setLayout(null);
+        add(marital);
+
+        JLabel address = new JLabel("Address:");
+        address.setFont(new Font("Raleway", Font.BOLD, 20));
+        address.setBounds(100, 440, 200, 30);
+        setLayout(null);
+        add(address);
+
+        JLabel city = new JLabel("City:");
+        city.setFont(new Font("Raleway", Font.BOLD, 20));
+        city.setBounds(100, 490, 200, 30);
+        setLayout(null);
+        add(city);
+
+        JLabel state = new JLabel("State:");
+        state.setFont(new Font("Raleway", Font.BOLD, 20));
+        state.setBounds(100, 530, 200, 30);
+        setLayout(null);
+        add(state);
+
+        JLabel pincode = new JLabel("Pin Code:");
+        pincode.setFont(new Font("Raleway", Font.BOLD, 20));
+        pincode.setBounds(100, 580, 200, 30);
+        setLayout(null);
+        add(pincode);
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(850, 800);
         setLocation(350, 10);
