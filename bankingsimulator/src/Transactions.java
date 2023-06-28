7d65d9a76dc90a0d287f2d7b6af8f2311f2c699c
MaxSlouka <jmslouka@gmail.com>
2023-06-28 20:59:41 +0200

    create buttons

--- bankingsimulator/src/Transactions.java
+++ bankingsimulator/src/Transactions.java
@@ -15,10 +15,39 @@ public class Transactions extends JFrame {
         add(image);
 
         JLabel text = new JLabel("Please select your Transaction");
-        text.setBounds(235, 300, 700, 35);
+        text.setBounds(210, 300, 700, 35);
         text.setForeground(Color.WHITE);
         text.setFont(new Font("System", Font.BOLD, 16));
         image.add(text);
+
+        JButton deposit = new JButton("Deposit");
+        deposit.setBounds(170, 415, 150, 30);
+        image.add(deposit);
+
+        JButton withdrawal = new JButton("Cash Withdrawal");
+        withdrawal.setBounds(355, 415, 150, 30);
+        image.add(withdrawal);
+
+        JButton fastCash  = new JButton("Fast Cash");
+        fastCash.setBounds(170, 450, 150, 30);
+        image.add(fastCash);
+
+        JButton ministatement = new JButton("Mini Statement");
+        ministatement.setBounds(355, 450, 150, 30);
+        image.add(ministatement);
+
+        JButton  pinchange = new JButton("Pin Change");
+        pinchange.setBounds(170, 485, 150, 30);
+        image.add(pinchange);
+
+        JButton  balanceenquiry = new JButton("Balance Enquiry");
+        balanceenquiry.setBounds(355, 485, 150, 30);
+        image.add(balanceenquiry);
+
+        JButton  exit = new JButton("Exit");
+        exit.setBounds(355, 520, 150, 30);
+        image.add(exit);
+
 
         setSize(900, 900);
         setLocation(300, 0);
