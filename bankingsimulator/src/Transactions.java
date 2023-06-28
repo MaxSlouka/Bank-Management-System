401f2199ab4bad4df3186d853671fb3294b8f553
MaxSlouka <jmslouka@gmail.com>
2023-06-28 20:49:57 +0200

    add font style, fix position

--- bankingsimulator/src/Transactions.java
+++ bankingsimulator/src/Transactions.java
@@ -5,14 +5,24 @@ public class Transactions extends JFrame {
 
     Transactions(){
 
+        setLayout(null);
+
         ImageIcon i1 = new ImageIcon("bankingsimulator\\src\\icons\\atm.jpg");
         Image i2 = i1.getImage().getScaledInstance(900, 900, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel image = new JLabel(i3);
+        image.setBounds(0, 0, 900, 900);
         add(image);
+
+        JLabel text = new JLabel("Please select your Transaction");
+        text.setBounds(235, 300, 700, 35);
+        text.setForeground(Color.WHITE);
+        text.setFont(new Font("System", Font.BOLD, 16));
+        image.add(text);
 
         setSize(900, 900);
         setLocation(300, 0);
+        setUndecorated(true);
         setVisible(true);
     }
 
