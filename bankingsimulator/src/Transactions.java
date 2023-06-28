e4889700a105f95bcff1c34562443597577fd5d9
MaxSlouka <jmslouka@gmail.com>
2023-06-28 20:45:03 +0200

    add background

--- bankingsimulator/src/Transactions.java
+++ bankingsimulator/src/Transactions.java
@@ -1,9 +1,19 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Transactions extends JFrame {
 
     Transactions(){
 
+        ImageIcon i1 = new ImageIcon("bankingsimulator\\src\\icons\\atm.jpg");
+        Image i2 = i1.getImage().getScaledInstance(900, 900, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        add(image);
+
+        setSize(900, 900);
+        setLocation(300, 0);
+        setVisible(true);
     }
 
     public static void main(String[] args) {
