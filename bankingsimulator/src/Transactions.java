35d8458d55aed7fe5f8e9ff189cdf4f5ba678e9d
MaxSlouka <jmslouka@gmail.com>
2023-06-28 21:02:57 +0200

    format code, add action listener

--- bankingsimulator/src/Transactions.java
+++ bankingsimulator/src/Transactions.java
@@ -1,50 +1,57 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Transactions extends JFrame {
+public class Transactions extends JFrame implements ActionListener {
 
-    Transactions(){
+    JButton deposit, withdrawal, fastCash, ministatement, pinchange, balanceenquiry, exit;
+    JLabel text, image;
+    ImageIcon i1, i3;
+    Image i2;
+
+    Transactions() {
 
         setLayout(null);
 
-        ImageIcon i1 = new ImageIcon("bankingsimulator\\src\\icons\\atm.jpg");
-        Image i2 = i1.getImage().getScaledInstance(900, 900, Image.SCALE_DEFAULT);
-        ImageIcon i3 = new ImageIcon(i2);
-        JLabel image = new JLabel(i3);
+        i1 = new ImageIcon("bankingsimulator\\src\\icons\\atm.jpg");
+        i2 = i1.getImage().getScaledInstance(900, 900, Image.SCALE_DEFAULT);
+        i3 = new ImageIcon(i2);
+        image = new JLabel(i3);
         image.setBounds(0, 0, 900, 900);
         add(image);
 
-        JLabel text = new JLabel("Please select your Transaction");
+        text = new JLabel("Please select your Transaction");
         text.setBounds(210, 300, 700, 35);
         text.setForeground(Color.WHITE);
         text.setFont(new Font("System", Font.BOLD, 16));
         image.add(text);
 
-        JButton deposit = new JButton("Deposit");
+        deposit = new JButton("Deposit");
         deposit.setBounds(170, 415, 150, 30);
         image.add(deposit);
 
-        JButton withdrawal = new JButton("Cash Withdrawal");
+        withdrawal = new JButton("Cash Withdrawal");
         withdrawal.setBounds(355, 415, 150, 30);
         image.add(withdrawal);
 
-        JButton fastCash  = new JButton("Fast Cash");
+        fastCash = new JButton("Fast Cash");
         fastCash.setBounds(170, 450, 150, 30);
         image.add(fastCash);
 
-        JButton ministatement = new JButton("Mini Statement");
+        ministatement = new JButton("Mini Statement");
         ministatement.setBounds(355, 450, 150, 30);
         image.add(ministatement);
 
-        JButton  pinchange = new JButton("Pin Change");
+        pinchange = new JButton("Pin Change");
         pinchange.setBounds(170, 485, 150, 30);
         image.add(pinchange);
 
-        JButton  balanceenquiry = new JButton("Balance Enquiry");
+        balanceenquiry = new JButton("Balance Enquiry");
         balanceenquiry.setBounds(355, 485, 150, 30);
         image.add(balanceenquiry);
 
-        JButton  exit = new JButton("Exit");
+        exit = new JButton("Exit");
         exit.setBounds(355, 520, 150, 30);
         image.add(exit);
 
@@ -57,5 +64,10 @@ public class Transactions extends JFrame {
 
     public static void main(String[] args) {
         new Transactions();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
